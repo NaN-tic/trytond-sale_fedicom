2d09a129fb6916e79e27b824558cc02a57109a1b
Albert Cervera i Areny <albert@nan-tic.com>
2018-10-24 00:16:20 +0200

    Do not use asterisk in imports.

--- __init__.py
+++ __init__.py
@@ -1,16 +1,14 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
-
 from trytond.pool import Pool
-from .sale import *
-from .configuration import *
-
+from . import sale
+from . import configuration
 
 def register():
     Pool.register(
-        Party,
-        Sale,
-        FedicomConfiguration,
-        FedicomConfigurationCompany,
-        FedicomLog,
+        sale.Party,
+        sale.Sale,
+        configuration.FedicomConfiguration,
+        configuration.FedicomConfigurationCompany,
+        sale.FedicomLog,
         module='sale_fedicom', type_='model')
