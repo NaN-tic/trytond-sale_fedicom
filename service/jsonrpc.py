fdc3d4d6668a66dfef0f541529757f90293cef02
Raimon Esteve <raimon@nan-tic.com>
2019-10-18 07:38:26 +0200

    FIX scenario test

--- service/jsonrpc.py
+++ service/jsonrpc.py
@@ -1,9 +1,6 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-try:
-    import io as StringIO
-except ImportError:
-    from io import StringIO
+from io import StringIO
 try:
     import simplejson as json
 except ImportError:
